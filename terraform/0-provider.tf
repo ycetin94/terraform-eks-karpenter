68b7cfb74d38ba33c4d618ae1f2f3eac88694ab6
ycetin94 <ycetin94@gmail.com>
2022-11-16 20:53:11 +0300

    terraform files configured

---
+++ terraform/0-provider.tf
@@ -0,0 +1,28 @@
+variable "cluster_name" {
+  default = "demo"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.39"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.6"
+    }
+
+    sonarqube = {
+      source = "jdamata/sonarqube"
+    }
+  }
+}
